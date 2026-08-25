19abe8916747874e6b7bd5a295ced7b8fa90ab29
clarence <clarencetwy@gmail.com>
2026-08-26 00:35:13 +0800

    implemented backend failover using non-blocking connect and poll

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,34 @@
 #include <unistd.h>
 #include <thread>
 #include <array>
-#include <vector>
 #include <atomic>
 #include <utility>
 #include <chrono>
+#include <fcntl.h>
+#include <poll.h>
+#include <optional>
+#include <functional>
 
 namespace{
+
+    constexpr std::uint16_t server_port = 8080;
+    //constexpr std::uint16_t backend_port = 9000;
+    constexpr int listen_backlog = 128; //how many pending connections may queue, waiting for accept()
+    constexpr std::size_t buffer_size = 4096;
+
+    constexpr auto backend_connect_timeout = std::chrono::milliseconds{1000};
+    constexpr auto health_check_timeout = std::chrono::milliseconds{500};
+
+    enum class ForwardDirection{
+        client_to_backend,
+        backend_to_client
+    };
+
+    [[noreturn]] void throw_system_error(const std::string& operation, int errorcode){
+        throw std::runtime_error(
+            operation + " failed " + std::strerror(errorcode)
+        );
+    }
 
     struct Backend{
         std::string address;
@@ -28,6 +50,11 @@ namespace{
             port{port},
             healthy{true}
         {}
+    };
+
+    struct BackendConnection{
+        Backend* backend;
+        int socket_fd;
     };
 
     std::array<Backend, 3> backends{
@@ -44,67 +71,164 @@ namespace{
         check_failed
     };
 
-    HealthCheckResult check_backend_health(const Backend& backend){
+    std::optional<int> try_connect_to_backend(const Backend& backend, std::chrono::milliseconds timeout){
         const int socket_fd = ::socket(AF_INET, SOCK_STREAM, 0);
 
         if(socket_fd == -1){
-            std::cerr
-                << "Health check socket creation failed: "
-                << std::strerror(errno)
-                << '\n';
-
-            return HealthCheckResult::check_failed;
+            throw_system_error("socket", errno);
         }
 
         sockaddr_in backend_address{};
         backend_address.sin_family = AF_INET;
         backend_address.sin_port = htons(backend.port);
 
-        int result = inet_pton(
+        const int address_result = inet_pton(
             AF_INET,
             backend.address.c_str(),
-            &backend_address.sin_addr
-        );
+            &backend_address.sin_addr);
 
-        if(result == 0){
+        if(address_result == 0){ //stored backend ip address is bad/invalid
+            //no need for errno cause address_reuslt == 0 means invalid ip. errno used if inet_pton returns -1
             ::close(socket_fd);
 
-            std::cerr
-                << "Invalid backend IP address: "
-                << backend.address
-                << '\n';
+            throw std::runtime_error(
+                "Invalid backend IP address: " + backend.address);
             
-            return HealthCheckResult::check_failed;
-
-        }else if(result == -1){
-            int errorcode = errno;
-            ::close(socket_fd);
-
-            std::cerr
-                << "inet_pton failed: "
-                << std::strerror(errorcode)
-                << '\n';
-
-            return HealthCheckResult::check_failed;
-
         }
 
-        if(::connect(
+        if(address_result == -1){
+            const int errorcode = errno;
+            ::close(socket_fd);
+
+            throw_system_error("inet_pton", errorcode);
+        }
+
+        const int original_flags = ::fcntl(socket_fd, F_GETFL, 0); //get socket's current file status-flags
+
+        if(original_flags == -1){
+            int errorcode = errno;
+            
+            ::close(socket_fd);
+            
+            throw_system_error("fcntl(F_GETFL)", errorcode);
+        }
+
+        if(::fcntl(
             socket_fd,
-            reinterpret_cast<sockaddr*>(&backend_address),
-            sizeof(backend_address)) == -1){
+            F_SETFL,
+            original_flags | O_NONBLOCK) == -1){
+                const int errorcode = errno;
+                
                 ::close(socket_fd);
 
+                throw_system_error("fcntl(F_SETFL)", errorcode);
+            }
+        
+
+        const int connect_result = ::connect(
+            socket_fd,
+            reinterpret_cast<sockaddr*>(&backend_address),
+            sizeof(backend_address)
+        );
+        
+        if(connect_result == -1 && errno != EINPROGRESS && errno != EINTR){
+            ::close(socket_fd);
+
+            return std::nullopt;
+        }
+
+        if(connect_result == -1){
+            pollfd poll_fd{};
+            poll_fd.fd = socket_fd;
+            poll_fd.events = POLLOUT;
+
+            const int poll_result = ::poll(&poll_fd, 1, static_cast<int>(timeout.count()));
+
+            if(poll_result == 0){
+                //timeout
+                ::close(socket_fd);
+                
+                return std::nullopt;
+            }
+
+            if(poll_result == -1){
+                const int errorcode = errno;
+
+                ::close(socket_fd);
+
+                if(errorcode == EINTR){
+                    return std::nullopt;
+                }else{
+                    throw_system_error("poll", errorcode);
+                }
+            }
+
+            int socket_error = 0;
+            socklen_t error_length = sizeof(socket_error);
+
+            if(::getsockopt(
+                socket_fd,
+                SOL_SOCKET,
+                SO_ERROR,
+                &socket_error,
+                &error_length) == -1){
+                    const int errorcode = errno;
+
+                    ::close(socket_fd);
+                    
+                    throw_system_error("getsockopt(SO_ERROR)", errorcode);
+                }
+            
+            if(socket_error != 0){
+                ::close(socket_fd);
+
+                return std::nullopt;
+            }
+        }
+
+            //socket_error == 0 or connect_result == 0 means connection successful
+            if(::fcntl(
+                socket_fd,
+                F_SETFL,
+                original_flags) == -1){
+                    const int errorcode = errno;
+                    
+                    ::close(socket_fd);
+                    
+                    throw_system_error("fcntl(F_SETFL)", errorcode);
+                }
+            
+            return socket_fd;
+    }
+
+    HealthCheckResult check_backend_health(const Backend& backend){
+        try{
+            const std::optional<int> socket_fd = try_connect_to_backend(backend, health_check_timeout);
+
+            if(!socket_fd.has_value()){
                 return HealthCheckResult::unhealthy;
             }
 
-        ::close(socket_fd);
+            ::close(*socket_fd);
+            
+            return HealthCheckResult::healthy;
 
-        return HealthCheckResult::healthy;
+        }catch(const std::exception& error){
+            std::cerr
+                << "Unable to perform health check for: "
+                << backend.address
+                << ':'
+                << backend.port
+                << ": "
+                << error.what()
+                << '\n';
+
+            return HealthCheckResult::check_failed;
+        }
 
     }
 
-    const Backend& choose_backend(){
+    Backend& choose_backend(){
         const std::size_t start_index = next_backend_index.fetch_add(
             1,
             std::memory_order_relaxed);
@@ -120,52 +244,41 @@ namespace{
         throw std::runtime_error("No healthy backends available");
     }
 
+    void set_backend_health(Backend& backend, bool is_healthy){
+        const bool previous_health = backend.healthy.exchange(
+            is_healthy,
+            std::memory_order_relaxed);
+        
+        if(previous_health != is_healthy){
+            std::cout
+                << "Backend "
+                << backend.address
+                << ':'
+                << backend.port
+                << " health changed. New status: "
+                << (is_healthy ? "healthy" : "unhealthy")
+                << '\n';
+        }
+    }
+
     void health_check_loop(){
         while(1){
             for(Backend& backend : backends){
                 HealthCheckResult result = check_backend_health(backend);
 
-                bool is_healthy{true};
-
                 if(result == HealthCheckResult::check_failed){
                     continue;
-
-                }else if(result == HealthCheckResult::unhealthy){
-                    is_healthy = false;
-
                 }
-                
-                const bool previous_health = backend.healthy.exchange(
-                    is_healthy,
-                    std::memory_order_relaxed);
 
-                if(is_healthy != previous_health){
-                    std::cout
-                        << "Backend "
-                        << backend.address
-                        << ':'
-                        << backend.port
-                        << " health changed. New status: "
-                        << (is_healthy ? "healthy" : "unhealthy")
-                        << '\n';
-                }
+                const bool is_healthy = result == HealthCheckResult::healthy;
+
+                set_backend_health(backend, is_healthy);
             }
 
             std::this_thread::sleep_for(
                 std::chrono::seconds{2});
 
         }
-    }
-
-    constexpr std::uint16_t server_port = 8080;
-    //constexpr std::uint16_t backend_port = 9000;
-    constexpr int listen_backlog = 128; //how many pending connections may queue, waiting for accept()
-    constexpr std::size_t buffer_size = 4096;
-
-    [[noreturn]] void throw_system_error(const std::string& operation, int errorcode){
-        throw std::runtime_error(
-            operation + " failed " + std::strerror(errorcode)
-        );
     }
 
     int create_listening_socket(std::uint16_t port){
@@ -261,39 +374,31 @@ namespace{
 
     }
 
-    int connect_to_backend(const Backend& backend){
-        const int socket_fd = ::socket(AF_INET, SOCK_STREAM, 0);
+    BackendConnection connect_to_healthy_backend(){
+        for(std::size_t attempts = 0; attempts < backends.size(); attempts++){
+            Backend& backend = choose_backend();
 
-        if(socket_fd == -1){
-            throw_system_error("socket", errno);
-        }
+            const std::optional<int> socket_fd = 
+                try_connect_to_backend(
+                    backend, 
+                    backend_connect_timeout);
+            
+            if(socket_fd.has_value()){
+                return BackendConnection{
+                    &backend,
+                    *socket_fd};
+            }
+            
+            //if socket_fd does not have value, this backend is unhealthy
+            set_backend_health(backend, false);
 
-        sockaddr_in backend_address{};
-        backend_address.sin_family = AF_INET;
-        backend_address.sin_port = htons(backend.port);
-
-        if(::inet_pton(
-            AF_INET,
-            backend.address.c_str(),
-            &backend_address.sin_addr) != 1){
-                ::close(socket_fd);
-                throw std::runtime_error("inet_pton failed");
-        }
-
-        if(::connect(
-            socket_fd,
-            reinterpret_cast<sockaddr*>(&backend_address),
-            sizeof(backend_address)) == -1){
-                int errorcode = errno;
-                ::close(socket_fd);
-                throw_system_error("connect", errorcode);
         }
         
-        return socket_fd;
+        throw std::runtime_error("Unable to connect to any healthy backend");
 
     }
 
-    void forward_data(int source_fd, int destination_fd){
+    void forward_data(int source_fd, int destination_fd, Backend& backend, ForwardDirection direction){
 
         try{
             std::array<char, buffer_size> buffer{};
@@ -310,8 +415,25 @@ namespace{
                     if(errno == EINTR){
                         continue;
                     }
+                    
+                    const int errorcode = errno;
 
-                    throw_system_error("recv", errno);
+                    if(direction == ForwardDirection::backend_to_client){
+
+                        switch(errorcode){
+                            case ECONNRESET:
+                            case ETIMEDOUT:
+                            case ENETRESET:
+                                set_backend_health(
+                                    backend,
+                                    false);
+                                break;
+
+                            default: break;
+                        }
+                    }
+
+                    throw_system_error("recv", errorcode);
                 }
 
                 if(bytes_received == 0){
@@ -342,7 +464,11 @@ namespace{
     }
 
     void proxy_connection(int client_fd){
-        const Backend& backend = choose_backend();
+        BackendConnection connection = connect_to_healthy_backend();
+
+        Backend& backend = *connection.backend;
+
+        const int backend_fd = connection.socket_fd;
 
         std::cout
             << "Selected backend: "
@@ -351,18 +477,20 @@ namespace{
             << backend.port
             << '\n';
 
-        const int backend_fd = connect_to_backend(backend);
-
         std::thread client_to_backend{
             forward_data,
             client_fd,
-            backend_fd
+            backend_fd,
+            std::ref(backend),
+            ForwardDirection::client_to_backend
         };
 
         std::thread backend_to_client{
             forward_data,
             backend_fd,
-            client_fd
+            client_fd,
+            std::ref(backend),
+            ForwardDirection::backend_to_client
         };
 
         client_to_backend.join();
